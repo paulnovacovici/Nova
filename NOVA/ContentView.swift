cf54533469b4f61c50fd7e0da6f2d392f140a670
Paul Novacovici <pnovacov@cisco.com>
2020-06-16 19:43:06 -0700

    Initial Commit

---
+++ NOVA/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NOVA
+//
+//  Created by pnovacov on 6/16/20.
+//  Copyright © 2020 Nova. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
